600d802a32e682f2499486ee23c95c9b0eb2bd0f
Roel de Cort <roel@decort.tech>
2024-04-13 21:40:46 +0200

    update catppuccin settings

--- config/plugins/themes/default.nix
+++ config/plugins/themes/default.nix
@@ -4,39 +4,41 @@ in {
   colorschemes = {
     catppuccin = {
       enable = true;
-      background = {
-        light = "macchiato";
-        dark = "mocha";
-      };
-      flavour = "frappe"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
-      disableBold = false;
-      disableItalic = false;
-      disableUnderline = false;
-      transparentBackground = true;
-      integrations = {
-        cmp = true;
-        noice = true;
-        notify = true;
-        gitsigns = true;
-        which_key = true;
-        illuminate = {
-          enabled = true;
+      settings = {
+        background = {
+          light = "macchiato";
+          dark = "mocha";
         };
-        treesitter = true;
-        treesitter_context = true;
-        telescope.enabled = true;
-        indent_blankline.enabled = true;
-        mini.enabled = true;
-        native_lsp = {
-          enabled = true;
-          inlay_hints = {
-            background = true;
+        flavour = "frappe"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+        no_bold = false;
+        no_italic = false;
+        no_underline = false;
+        transparent_background = true;
+        integrations = {
+          cmp = true;
+          noice = true;
+          notify = true;
+          gitsigns = true;
+          which_key = true;
+          illuminate = {
+            enabled = true;
           };
-          underlines = {
-            errors = ["underline"];
-            hints = ["underline"];
-            information = ["underline"];
-            warnings = ["underline"];
+          treesitter = true;
+          treesitter_context = true;
+          telescope.enabled = true;
+          indent_blankline.enabled = true;
+          mini.enabled = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
           };
         };
       };
