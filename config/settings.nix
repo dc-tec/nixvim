b59a4d5afc31b1a1dcd7f9b84a63dcffb88e2cbb
Roel de Cort <roel@decort.tech>
2024-04-20 15:28:07 +0200

    update vim options

--- config/settings.nix
+++ config/settings.nix
@@ -3,18 +3,19 @@
     opts = {
       number = true;
       relativenumber = true;
-
       tabstop = 2;
       softtabstop = 2;
       showtabline = 2;
       expandtab = true;
-
       smartindent = true;
       shiftwidth = 2;
-
       breakindent = true;
-
       cursorline = true;
+      scrolloff = 8;
+      foldmethod = "manual";
+      foldenable = false;
+      linebreak = true;
+      spell = false;
     };
   };
 }
