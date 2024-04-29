942567fdc09e49e4377677801e394d4710116b2b
Roel de Cort <roel@decort.tech>
2024-04-29 11:00:59 +0200

    update keymap

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -39,7 +39,7 @@
         };
 
         mapping = {
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<leader><Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
           "<C-j>" = "cmp.mapping.select_next_item()";
           "<C-k>" = "cmp.mapping.select_prev_item()";
           "<C-e>" = "cmp.mapping.abort()";
