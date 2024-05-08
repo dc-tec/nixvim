cd883078473091e78393f683b964d61b4bb53925
Roel de Cort <roel@decort.tech>
2024-05-09 01:34:39 +0200

    add autoCmd's

--- config/auto_cmds.nix
+++ config/auto_cmds.nix
@@ -1,11 +1,33 @@
 {
+  autoGroups = {
+    highlight_yank = {};
+    vim_enter = {};
+  };
+
   autoCmd = [
     {
       group = "highlight_yank";
-      callback = ''
-        function()
-          vim.highlight.on_yank()
-      '';
+      event = ["TextYankPost"];
+      pattern = "*";
+      callback = {
+        __raw = ''
+          function()
+            vim.highlight.on_yank()
+          end
+        '';
+      };
+    }
+    {
+      group = "vim_enter";
+      event = ["VimEnter"];
+      pattern = "*";
+      callback = {
+        __raw = ''
+          function()
+            vim.cmd('Startup')
+          end
+        '';
+      };
     }
   ];
 }
