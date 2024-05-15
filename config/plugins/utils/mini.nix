bfc5703e1d4ba79298e9c49e09e61371a93fd2c2
Roel de Cort <roel@decort.tech>
2024-05-16 01:19:49 +0200

    add mini plugin

---
+++ config/plugins/utils/mini.nix
@@ -0,0 +1,15 @@
+{
+  plugins.mini = {
+    enable = true;
+
+    modules = {
+      indentscope = {
+        symbol = "│";
+        options = {
+          try_as_border = true;
+        };
+      };
+      surround = {};
+    };
+  };
+}
