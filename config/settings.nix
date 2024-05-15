78f7597afe396a08e4d4262ee057db43939da03d
Roel de Cort <roel@decort.tech>
2024-05-16 01:20:37 +0200

    add diagnostic error icons

--- config/settings.nix
+++ config/settings.nix
@@ -1,8 +1,18 @@
 {
   config = {
+    extraConfigLuaPre =
+      # lua
+      ''
+        vim.fn.sign_define("diagnosticsignerror", { text = " ", texthl = "diagnosticerror", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsignwarn", { text = " ", texthl = "diagnosticwarn", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsignhint", { text = "󰌵", texthl = "diagnostichint", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsigninfo", { text = " ", texthl = "diagnosticinfo", linehl = "", numhl = "" })
+      '';
+
     clipboard = {
       providers.wl-copy.enable = true;
     };
+
     opts = {
       number = true;
       relativenumber = true;
