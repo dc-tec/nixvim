a6ff6f27e50ffa18bd5b802c15b5f881e6737001
Roel de Cort <roel@decort.tech>
2024-05-28 22:34:22 +0200

    check if we stop getting random crashes

--- config/plugins/snippets/luasnip.nix
+++ config/plugins/snippets/luasnip.nix
@@ -1,15 +1,9 @@
-{pkgs, ...}: {
+_: {
   plugins.luasnip = {
     enable = true;
     extraConfig = {
       enable_autosnippets = true;
       store_selection_keys = "<Tab>";
     };
-    fromVscode = [
-      {
-        lazyLoad = true;
-        paths = "${pkgs.vimPlugins.friendly-snippets}";
-      }
-    ];
   };
 }
