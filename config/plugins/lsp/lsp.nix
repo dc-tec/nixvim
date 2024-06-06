677ee2cd465567436c7c33509ee6e626a585fe06
Roel de Cort <roel@decort.tech>
2024-06-07 00:36:21 +0200

    fix yaml schemas

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -17,22 +17,22 @@
         yamlls = {
           enable = true;
           extraOptions = {
-            settings = ''
+            settings = {
               yaml = {
                 schemas = {
-                  kubernetes = '*.yaml',
-                  ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
-                  ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
-                  ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
-                  ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
-                  ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
-                  ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
-                  ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
-                  ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
-                  ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
-                },
-              }
-            '';
+                  kubernetes = "'*.yaml";
+                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
+                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
+                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
+                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
+                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
+                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
+                };
+              };
+            };
           };
         };
       };
