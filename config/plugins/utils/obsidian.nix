e98201179ff68a42a539918bf1f9f53da312085b
Roel de Cort <roel@decort.tech>
2024-06-23 00:23:51 +0200

    configure obsidian

---
+++ config/plugins/utils/obsidian.nix
@@ -0,0 +1,25 @@
+{
+  plugins.obsidian = {
+    enable = true;
+    settings = {
+      workspaces = [
+        {
+          name = "SecondBrain";
+          path = "~/projects/personal/SecondBrain";
+        }
+      ];
+      templates = {
+        subdir = "templates";
+        dateFormat = "%Y-%m-%d";
+        timeFormat = "%H:%M";
+        substitutions = {};
+      };
+
+      dailyNotes = {
+        folder = "0_Daily_Notes";
+        dateFormat = "%Y-%m-%d";
+        aliasFormat = "%B %-d, %Y";
+      };
+    };
+  };
+}
