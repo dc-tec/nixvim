c7b5e01292b03f97b0bc4d75e0732a121df6753b
Roel de Cort <roel@decort.tech>
2024-07-25 22:54:55 +0200

    add autocmd to restore cursor on reopen

--- config/auto_cmds.nix
+++ config/auto_cmds.nix
@@ -3,6 +3,7 @@
     highlight_yank = {};
     vim_enter = {};
     indentscope = {};
+    restore_cursor = {};
   };
 
   autoCmd = [
@@ -46,6 +47,26 @@
         __raw = ''
           function()
             vim.b.miniindentscope_disable = true
+          end
+        '';
+      };
+    }
+    ## from NVChad https://nvchad.com/docs/recipes (this autocmd will restore the cursor position when opening a file)
+    {
+      group = "restore_cursor";
+      event = ["BufReadPost"];
+      pattern = "*";
+      callback = {
+        __raw = ''
+          function()
+            if
+              vim.fn.line "'\"" > 1
+              and vim.fn.line "'\"" <= vim.fn.line "$"
+              and vim.bo.filetype ~= "commit"
+              and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+            then
+              vim.cmd "normal! g`\""
+            end
           end
         '';
       };
