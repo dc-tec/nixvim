0e002373244f6958a55e6398a9a984b1c1c09638
Roel de Cort <roel@decort.tech>
2024-08-24 22:34:52 +0200

    renamed settings

--- config/plugins/editor/todo-comments.nix
+++ config/plugins/editor/todo-comments.nix
@@ -1,12 +1,14 @@
 _: {
   plugins.todo-comments = {
     enable = true;
-    colors = {
-      error = ["DiagnosticError" "ErrorMsg" "#ED8796"];
-      warning = ["DiagnosticWarn" "WarningMsg" "#EED49F"];
-      info = ["DiagnosticInfo" "#EED49F"];
-      default = ["Identifier" "#F5A97F"];
-      test = ["Identifier" "#8AADF4"];
+    settings = {
+      colors = {
+        error = ["DiagnosticError" "ErrorMsg" "#ED8796"];
+        warning = ["DiagnosticWarn" "WarningMsg" "#EED49F"];
+        info = ["DiagnosticInfo" "#EED49F"];
+        default = ["Identifier" "#F5A97F"];
+        test = ["Identifier" "#8AADF4"];
+      };
     };
   };
 }
