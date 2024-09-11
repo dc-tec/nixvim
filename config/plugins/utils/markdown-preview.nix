73018a2f0858caecc01844ec4c6899fc03eda217
Roel de Cort <roel@decort.tech>
2024-09-11 20:39:27 +0200

    fixed deprecated options

--- config/plugins/utils/markdown-preview.nix
+++ config/plugins/utils/markdown-preview.nix
@@ -4,11 +4,11 @@ _: {
       enable = true;
       settings = {
         browser = "firefox";
-        echo_preview_url = true;
+        echo_preview_url = 1;
         port = "6969";
         preview_options = {
-          disable_filename = true;
-          disable_sync_scroll = true;
+          disable_filename = 1;
+          disable_sync_scroll = 1;
           sync_scroll_type = "middle";
         };
         theme = "dark";
