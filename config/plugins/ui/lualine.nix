b7d1a54e43d72121b627a9587d0dd942c2b55279
Roel de Cort <63876068+dc-tec@users.noreply.github.com>
2024-09-11 22:43:40 +0200

    patch: fixes (#34)
    - Fixed Luanne, as it was not configured correctly after the last
    refactor (should've tested it correctly)
    - Fixed small issue with separator in neo-tree
    - Remove EOB character

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -12,90 +12,82 @@ _: {
           statusline = ["startup" "alpha"];
         };
         theme = "catppuccin";
-        sections = {
-          lualine_a = [
-            {
-              name = "mode";
-              icon = " ";
-            }
-          ];
-          lualine_b = [
-            {
-              name = "branch";
-              icon = "";
-            }
-            {
-              name = "diff";
-              extraConfig = {
-                symbols = {
-                  added = " ";
-                  modified = " ";
-                  removed = " ";
-                };
-              };
-            }
-          ];
-          lualine_c = [
-            {
-              name = "diagnostics";
-              extraConfig = {
-                sources = ["nvim_lsp"];
-                symbols = {
-                  error = " ";
-                  warn = " ";
-                  info = " ";
-                  hint = "󰝶 ";
-                };
-              };
-            }
-            {
-              name = "navic";
-            }
-          ];
-          lualine_x = [
-            {
-              name = "filetype";
-              extraConfig = {
-                icon_only = true;
-                separator = "";
-                padding = {
-                  left = 1;
-                  right = 0;
-                };
-              };
-            }
-            {
-              name = "filename";
-              extraConfig = {
-                path = 1;
-              };
-            }
-            {
-              name.__raw = ''
-                function()
-                  local icon = " "
-                  local status = require("copilot.api").status.data
-                  return icon .. (status.message or " ")
-                end,
+      };
+      sections = {
+        lualine_a = [
+          {
+            __unkeyed-1 = "mode";
+            icon = "";
+          }
+        ];
+        lualine_b = [
+          {
+            __unkeyed-1 = "branch";
+            icon = "";
+          }
+          {
+            __unkeyed-1 = "diff";
+            symbols = {
+              added = " ";
+              modified = " ";
+              removed = " ";
+            };
+          }
+        ];
+        lualine_c = [
+          {
+            __unkeyed-1 = "diagnostics";
+            sources = ["nvim_lsp"];
+            symbols = {
+              error = " ";
+              warn = " ";
+              info = " ";
+              hint = "󰝶 ";
+            };
+          }
+          {
+            __unkeyed-1 = "navic";
+          }
+        ];
+        lualine_x = [
+          {
+            __unkeyed-1 = "filetype";
+            icon_only = true;
+            separator = "";
+            padding = {
+              left = 1;
+              right = 0;
+            };
+          }
+          {
+            __unkeyed-1 = "filename";
+            path = 1;
+          }
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                local icon = " "
+                local status = require("copilot.api").status.data
+                return icon .. (status.message or " ")
+              end,
 
-                cond = function()
-                 local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
-                 return ok and #clients > 0
-                end,
-              '';
-            }
-          ];
-          lualine_y = [
-            {
-              name = "progress";
-            }
-          ];
-          lualine_z = [
-            {
-              name = "location";
-            }
-          ];
-        };
+              cond = function()
+               local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+               return ok and #clients > 0
+              end,
+            '';
+          }
+        ];
+        lualine_y = [
+          {
+            __unkeyed-1 = "progress";
+          }
+        ];
+        lualine_z = [
+          {
+            __unkeyed-1 = "location";
+          }
+        ];
       };
     };
   };
