284cee30ea23b1861d64c37ec500394ca1e56356
Roel de Cort <roel@decort.tech>
2024-11-26 10:02:25 +0100

    update gitignore and fixed issue in flake.nix

--- flake.nix
+++ flake.nix
@@ -28,39 +28,44 @@
         "x86_64-darwin"
       ];
 
-      perSystem = {
-        system,
-        pkgs,
-        self',
-        lib,
-        ...
-      }: let
-        nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
-        nixvimModule = {
-          inherit pkgs;
-          module = import ./config; # import the module directly
-          # You can use `extraSpecialArgs` to pass additional arguments to your module files
-          extraSpecialArgs = {
-            # inherit (inputs) foo;
+      perSystem =
+        {
+          system,
+          pkgs,
+          self',
+          lib,
+          ...
+        }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit pkgs;
+            module = import ./config; # import the module directly
+            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            extraSpecialArgs = {
+              # inherit (inputs) foo;
+            };
           };
-        };
-        nvim = nixvim'.makeNixvimWithModule nixvimModule;
-      in
-      {
-        checks = {
-          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              statix.enable = true;
-              nixfmt-rfc-style.enable = true;
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in
+        {
+          checks = {
+            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                statix.enable = true;
+                nixfmt-rfc-style.enable = true;
+              };
             };
           };
 
           formatter = pkgs.nixfmt-rfc-style;
 
-          packages.default = nvim;
+          packages = {
+            default = nvim;
+          };
 
           devShells = {
             default = with pkgs; mkShell { inherit (self'.checks.pre-commit-check) shellHook; };
